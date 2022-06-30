bb32a76841d4615eb2f0770ce807add51784ecff
Ha0203 <mr.stsh020303@gmail.com>
2022-06-30 10:47:17 +0700

    Merge vô branch

---
+++ Main/Main/Header.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    string data;
+    node* left;
+    node* right;
+};
+
+// Function List
+node* buildExpressionTree(string infix);
+void printTree(node* root);
+void removeTree(node*& root);
---
+++ Main/Main/Main.cpp
@@ -0,0 +1,10 @@
+#include"Header.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
